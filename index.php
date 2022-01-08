64b11d2ca4cdc4c67826152a3c06cab1f5854d11
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-01-08 18:18:49 +0545

    wp theme started

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/*
+* This is My First Theme
+*/
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>RKY Theme</title>
+</head>
+<body>
+<h1>Hi i'm Rabi Kr yadav</h1>
+</body>
+</html>
