7a5b05d2c847eeebdfc71b2c38fc8345ffc58d75
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-01-11 20:59:30 +0545

    single page layout

--- single.php
+++ single.php
@@ -4,12 +4,27 @@
 */
 get_header(); // this function for include header file
 ?>
-<div class="container">
-	<?php get_template_part('template-parts/homeFeatured') ?>
+<div class="container post-content">
 	<?php if (have_posts() ) : ?>
 	<?php while( have_posts()) : the_post(); ?>
-	<h1><?php the_title() ?></h1>
-	<p><?php the_content(); ?></p>
+		<div class="post-image">
+			<?php the_post_thumbnail('home-featured') ?>
+		</div>
+		<div class="post-title">
+			<h1><?php the_title() ?></h1>		
+		</div>
+		<div class="post-meta-row">
+			<div class="post-meta">
+				<strong>Author: </strong><?php the_author(); ?>
+			</div>
+			<div class="post-meta">
+				<strong>Posted on: </strong><?php the_time(); ?>
+			</div>
+		</div>
+		<div class="post-content">
+			<p><?php the_content(); ?></p>
+		</div>
+		
 	<?php endwhile; ?>
 	<?php endif; ?>
 </div>
